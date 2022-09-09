5f99a7ffb629666d195bce5141b0bc1ec9f8a895
maxogod <maxi18wars@gmail.com>
2022-09-08 22:19:40 -0300

    init budget app

---
+++ budget-app/budget.py
@@ -0,0 +1,26 @@
+class Category:
+    
+    def __init__(self, category_name) -> None:
+        self.ledger = []
+        self.name = category_name
+    
+    def deposit():
+        pass
+    
+    def withdraw():
+        pass
+    
+    def get_balance():
+        pass
+    
+    def transfer():
+        pass
+    
+    def check_funds():
+        pass
+    
+    pass
+
+
+def create_spend_chart(categories):
+    pass
